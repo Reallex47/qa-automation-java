84165ac67b3db43e36664f2a33b9de454c11e040
Reallex47 <39802825+Reallex47@users.noreply.github.com>
2021-08-25 13:16:14 +0300

    Задание 5. Преобразовал статические классы в экземляры Controller, Service, Repository. Создал Request, Response. fix по замечаниям

--- app/src/main/java/com/tinkoff/edu/app/model/LoanRequest.java
+++ app/src/main/java/com/tinkoff/edu/app/model/LoanRequest.java
@@ -2,8 +2,11 @@ package com.tinkoff.edu.app.model;
 
 import com.tinkoff.edu.app.types.LoanType;
 
+/**
+ * TODO Class, type -> objects, instances
+ */
 public class LoanRequest {
-    private final int months; //stateful + immutable
+    private final int months;
     private final int amount;
     private final LoanType type;
 
